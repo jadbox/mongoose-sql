1ad6de72a0f90bdd48b9502737f733a3387eaf27
Jonathan Dunlap <jdunlap@outlook.com>
2016-12-27 16:06:41 -0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const sequelize = require('sequelize');
+
+const monSchema = mongoose.Schema
+
+function Schema(params) {
+
+}
+
+function Model() {
+
+}
+
+Model.prototype.find = function(params) {
+
+}
+
+Model.prototype.findByID = function(id) {
+
+}
+
+// Returns Model
+function model(name, schema) {
+
+}
+
+module.exports = exports = {
+  Schema: Schema
+  model: model
+}
