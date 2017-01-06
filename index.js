bba3d0d2951d2aca7bf827e0abdd1ef4c906cbaf
Jonathan Dunlap <jdunlap@outlook.com>
2017-01-05 19:11:01 -0800

    lazy loadded schemas done

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ class Schema {
       .mapValues(x => ({type:typeMap[x.type]})).value();
 
     const hasOneType = _(params).pickBy(x=>x.ref)
-      .mapValues(x => ({one:x.ref})).value();
+      .mapValues(x => ({ref:x.ref, rel:'one'})).value();
 
     // Default value conversion (non-collection)
     const vDefaults = _(params).pickBy(x=>x.default)
@@ -62,16 +62,21 @@ class Schema {
     // Collections with schema ref
     const hasManyTypes = _(params).pickBy(isArrayType)
       .pickBy(x => x[0].ref)
-      .mapValues(x => ({many:x[0].ref})).value();
+      .mapValues(x => ({ref:x[0].ref, rel: 'many'})).value();
 
     // Defaults on array types
     const vADefaults = _(params).pickBy(isArrayType)
       .pickBy(x=>x[0].default)
       .mapValues(x => ({defaultValue: x[0].default})).value();
 
-    const v = _.merge(vTypes, vATypes, vADefaults, vUnique, vRequired);
+    // Lowercase restrictions on array types
+    const vLowerCase = _(params).pickBy(x=>x.lowercase)
+      .mapValues(x => ({validate: { isLowercase: true }})).value();
+
+    const v = _.merge(vTypes, vATypes, vADefaults, vUnique, vRequired, vLowerCase);
     const refs = _.merge(hasOneType, hasManyTypes);
     v.refs = refs;
+    //console.log(v);
     return v;
   }
 };
@@ -80,12 +85,30 @@ class Model {
   constructor(name, schema) {
     this.name = name;
     console.log('-- parsing ' + name + ' --');
-    this.sqlm = schema.parse(); // change to build
-    this.refs = this.sqlm.refs;
-    delete this.sqlm.refs;
+    this.schema = schema.parse(); // change to build
+    this.refsUnlinked = _.map(this.schema.refs, x => x.ref);
+    this.refs = _.merge({}, this.schema.refs);
+    delete this.schema.refs;
 
-    console.log(this.sqlm);
+    console.log(this.schema);
     console.log('refs', this.refs);
+  }
+  // Creates the Sequelize models
+  _sqlize(models) {
+    // sequelize model
+
+    _(this.refs).pickBy(x=>x.rel==='many')
+      .map( (k,v) => {
+        // hasMany( models[v.ref] )
+        return k;
+      }).value();
+
+    _(this.refs).pickBy(x=>x.rel==='one')
+      .map( (k,v) => {
+        // hasOne( models[v.ref] )
+        return k;
+      }).value();
+
   }
   find(params) {
     return sqlm.findAll();
@@ -108,7 +131,24 @@ class Model {
 function modelNew(name, schema) {
   const model = new Model(name, schema);
   models[name] = model; // cache it
+  waitOn(model);
   return model;
+}
+
+// Check for dependency models added
+function waitOn(model) {
+  let ks = model.refsUnlinked;
+  if(ks.length === 0) {
+    console.log(model.name + ' has all deps');
+    model._sqlize(models);
+    return;
+  }
+  const loaded = _.intersection(ks, _.keys(models));
+  
+  model.refsUnlinked = ks = _.difference(model.refsUnlinked, loaded);
+
+  console.log(model.name + ' loaded', loaded, 'wating on', ks);
+  setTimeout(waitOn, 1000, model);
 }
 
 module.exports = exports = {
