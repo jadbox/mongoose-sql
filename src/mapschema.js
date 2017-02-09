f17f41df7f1575f3d930c133ef9fbfafbb1c6ae9
Jonathan Dunlap <jdunlap@outlook.com>
2017-02-08 22:12:07 -0800

    fixed default param sync

--- src/mapschema.js
+++ src/mapschema.js
@@ -7,9 +7,7 @@ module.exports = {
   parse,
   sync,
   find,
-  findByID,
-  create,
-  migrateSchemas
+  findByID
 };
 
 const TYPE_JSONB = 'jsonb';
@@ -27,7 +25,7 @@ const typeMap = {
 
 const ONE = '1', MANY = '>1';
 
-const ARRAY_OBJ_TYPE = typeMap[Object];
+const ARRAY_OBJ_TYPE = typeMap[Array];
 const isArrayType = x => Array.isArray(x) && x.length !== 0;
 
 // Builds the name of the join table
@@ -104,7 +102,7 @@ function parse(name, params, knex) {
       if (x.default === Date.now)
         return { notNullable: true, default: knexNow() };
       else
-        return { default: x.default };
+        return { notNullable: true, default: x.default };
     })
     .value();
 
@@ -125,7 +123,7 @@ function parse(name, params, knex) {
   const vATypes = _(params)
     .pickBy(isArrayType)
     .pickBy(x => !x[0].ref)
-    .mapValues(x => ({ type: ARRAY_OBJ_TYPE }))
+    .mapValues(x => ({ type: ARRAY_OBJ_TYPE, notNullable: true, default: '[]' }))
     .value();
 
   // Defaults on array types
@@ -198,8 +196,7 @@ function sync(knex, _schema) {
           console.warn(_schema.table + ': lacks type for prop ' + v.type);
           return;
         }
-
-        if (v.defaut) z = z.defaultTo(v.default);
+        if (v.default) z = z.defaultTo(v.default);
         if (v.notNullable) z = z.notNullable();
       });
     });
@@ -246,218 +243,4 @@ function find(knex, _schema) {
 function findByID(knex, _schema, id) {
   const q = find(knex, _schema);
   return q.where('_id', id);
-}
-
-// Create an entry with associations
-function create(knex, _schema, obj) {
-  if (obj.recommendedPackages) {
-    //console.log('_schema.joins', _schema.joins, _.keys(obj));
-    console.log(_(obj).pickBy((v, k) => _schema.joins[k]).value());
-    //return;
-  }
-
-  const filtered = removeInvalidKeys(_schema, obj);
-  const jsonbFixed = correctJsonFields(_schema, filtered);
-
-  //console.log(_schema.table + " saving ");
-  // + JSON.stringify(jsonbFixed));
-  let query = knex(_schema.table).insert(jsonbFixed).returning('_id');
-
-  // associations
-  _(obj)
-    .pickBy((v, k) => _schema.joins[k])
-    .mapValues((v, k) => {
-      const vo = _schema.joins[k];
-      console.log('---------saving into ', vo.ltable, vo.refTable);
-      _.forEach(v, vid => {
-        query = query.then(ids => // return row id
-          knex(vo.ltable)
-            .insert({ [vo.refTable]: ids[0], [k]: vid })
-            .then(x => ids));
-      });
-    })
-    .value();
-
-  return query;
-}
-
-// ===== migrateTable helpers =====
-// stringify jsonb fields
-function correctJsonFields(_schema, obj) {
-  const r = _(obj)
-    .pickBy((v, k) => _schema.props[k] && _schema.props[k].type === TYPE_JSONB)
-    .mapValues(JSON.stringify)
-    .value();
-
-  return _.merge(obj, r); //l->r
-}
-
-function removeInvalidKeys(_schema, obj) {
-  const w = _.without(_.keys(obj), '__id', ..._schema.fields);
-
-  // warning on non-matching field
-  if (w.length > 1 && !_schema.fieldWarning) {
-    console.log(_schema.table + ' removed fields', _.without(w, '__v'));
-    _schema.fieldWarning = true;
-  }
-
-  // take only valid fields
-  return filtered = _.omit(obj, '__v', ...w);
-}
-
-// Move Mongo's _id field to __id
-function moveIDKey(obj) {
-  // in-place op
-  //if (typeof obj._id === 'object' || typeof obj._id === 'string') {
-  obj.__id = obj._id.toString();
-  delete obj._id;
-  // }
-  return obj;
-}
-
-// Cheating global state that persist over several table migrations
-const idMap = {}, todo = [];
-//batchInsert
-function migrateTable(knex, _schema, objs) {
-  const _removeInvalidKeys = removeInvalidKeys.bind(null, _schema),
-    _correctJsonFields = correctJsonFields.bind(null, _schema);
-
-  objs = _.map(objs, x => x.toObject ? x.toObject() : x);
-
-  //schemaMap[_schema.table] = _schema;
-  objs = _.map(objs, v => moveIDKey(v));
-
-  _.map(_schema.refs, field => _.map(objs, o => {
-    // map over schema refs // extract fields
-    //console.log('=======k', field);
-    //process.exit(1);
-    const val = o[field];
-    if (!val && val !== 0) return;
-    if (_.isObject(val) || _.isArray(val))
-      if (val.length && val.length === 0) return;
-
-    delete o[field];
-
-    // removed from insert
-    let todoItem = { field, id: o.__id, val, table: _schema.table };
-
-    // save association table for many to many
-    if (_schema.joins[field]) {
-      todoItem = _.merge(todoItem, {
-        refTable: _schema.joins[field].refTable,
-        ltable: _schema.joins[field].ltable
-      });
-    }
-    todo.push(todoItem);
-  }));
-
-  const filtered = _.map(objs, _removeInvalidKeys);
-  const jsonbFixed = _.map(filtered, _correctJsonFields);
-
-  console.log(_schema.table + ' saving ' + jsonbFixed.length + ' rows');
-  console.log();
-  let query = Promise.all(
-    _.map(jsonbFixed, o => {
-      return knex(_schema.table)
-        .insert(o)
-        .returning(['_id', '__id'])
-        .then(x => x); //toPromise
-    })
-  ).then(results => {
-    // save id map
-    _.forEach(results, ([{ _id, __id }]) => idMap[__id] = _id);
-    //console.log(_schema.table, "idMap", idMap);
-    return results;
-  });
-
-  return query;
-}
-
-// Saves associations
-function migrateTablePost(knex) {
-  const todoMap = _.map(_.cloneDeep(todo), e => {
-    e.id = idMap[e.id];
-    if (!e.id) throw new Error('inconsistent id record', e.id);
-
-    // direct relationships id mapping
-    if (!e.refTable) {
-      e.val = idMap[e.val];
-      if (!e.val) {
-        console.warn(e);
-        throw new Error('Ref inconsistent record ' + e.val);
-      }
-    } else {
-      // non-direct many-many relationships id mapping
-      e.val = _(e.val)
-        .map(val => {
-          // one relationship
-          const id = idMap[val];
-          if (!id) console.warn('RefList inconsistent record ' + val);
-          return id || null;
-        })
-        .filter(null)
-        .value();
-    }
-    return e;
-  });
-
-  // todo many to many
-  const ps = _.map(todoMap, e => {
-    //const _schema = schemaMap[e.table];
-    if (!e.refTable) {
-      return knex(e.table)
-        .where('_id', e.id)
-        .update(e.field, e.val)
-        .returning('_id')
-        .then(x => x);
-    } else {
-      // save associations
-      return Promise.all(_.map(e.val, val => {
-        return knex(e.ltable)
-          .insert({ [e.field]: val, [e.table]: e.id })
-          .returning([[e.field], [e.table]])
-          .then(x => x);
-      }));
-    }
-  });
-
-  return Promise.all(ps);
-}
-
-// parse(name, params, knex) {
-function migrateSchemas(knex, mongoose, schemas) {
-  const _migrateSchema = migrateSchema.bind(null, knex, mongoose);
-
-  let q = _.reduce(schemas, (qq, s) => {
-    return qq.then(() => _migrateSchema(s));
-  }, Promise.resolve());
-
-  q = q.then(y => {
-    console.log('non-relational schemas migrated');
-    return migrateTablePost(knex).then(x => console.log('all schemas migrated'));
-  });
-  return q;
-}
-
-// TODO cleanup: move to migration
-function migrateSchema(knex, mongoose, Base) {
-  console.log('migrating', Base.table);
-  return Promise.resolve().then(() => Base.mongoose.find().exec()).then(x => {
-
-      /*if(Base.table.indexOf('ackage') > -1) {
-        console.log('catching Package');
-        _(x).filter(y=>y.pkgStatsVisibility)
-          .map(y=>console.log(y)).value();
-        return;
-      }*/
-      //console.log(Base.table, x);
-
-      if (!x || !x.length) throw new Error('no length on ' + x);
-      return migrateTable(knex, Base, x).then(x1 => {
-        if (!x) x = [];
-        console.log(Base.table + ' migrated rows ' + x.length);
-        //resolve('done');
-      });
-    });
-
 }
